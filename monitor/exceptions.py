b2b9f92eb48843c0ea949ae8f01d5fae0e6fd9c8
Leonardo Arroyo <contato@leonardoarroyo.com>
2017-07-05 11:58:47 -0300

    Add basic exceptions

---
+++ monitor/exceptions.py
@@ -0,0 +1,17 @@
+class PerformanceProblems():
+  """
+  Raised by assertions when a performance problem is detected
+  """
+  pass
+
+class PartialOutage():
+  """
+  Raised by assertions when there is a partial outage in a component
+  """
+  pass
+
+class CompleteOutage():
+  """
+  Raised by assertions when there is a complete outage in a component
+  """
+  pass
