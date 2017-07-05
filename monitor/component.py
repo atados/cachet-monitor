751f77c792dc4bc1ee0f25ffa67676697c21a1a9
Leonardo Arroyo <contato@leonardoarroyo.com>
2017-07-05 17:17:19 -0300

    Implement assertions testing and notifications

--- monitor/component.py
+++ monitor/component.py
@@ -1,16 +1,23 @@
 from .utils import import_from_string
 from .worker import WorkerThread
+from .status import *
+import threading
 import shortuuid
+import logging
 
 class Component():
   def __init__(self, name, assertions):
     self.name = name
     self.assertions = assertions
+    self.history = {}
+    self.last_status = None
+    self.logger = logging.getLogger('monitor')
 
   def start(self):
     """
     Start threads for all assertions declared for a given component.
     """
+    lock = threading.Lock()
     for assertion in self.assertions:
       AssertionClass = import_from_string(assertion["assertion"])
       kwargs = assertion.get("kwargs", {})
@@ -22,5 +29,59 @@ class Component():
       name = settings.get("name", shortuuid.uuid())
       assertion_object.set_friendly_name(name)
 
-      thread = WorkerThread(assertion_object, interval=settings.get("interval", 1*60))
+      thread = WorkerThread(
+        assertion_object,
+        assertion_executed_event=self.assertion_executed_event,
+        interval=settings.get("interval", 1*60),
+        lock=lock
+      )
       thread.start()
+
+  def assertion_executed_event(self, uuid, result):
+    """
+    This method is called by the Worker thread everyime a asserntion is ran
+    to inform the component about the result of the test.
+
+    Please note we call self.update_component_status which modifies shared data,
+    therefore we need to acquire and release a lock when calling it.
+    """
+    self.history[uuid] = result
+    self.update_component_status()
+
+  def update_component_status(self):
+    """
+    Calls the API to update the component status if it has changed
+    """
+    status = self.determine_component_status()
+    if status != self.last_status:
+      self.logger.info("Component {} status has changed to {}".format(self.name, status))
+      self.last_status = status
+
+  def determine_component_status(self):
+    """
+    Determine the component status by the history of tests
+    """
+    data = {
+      ASSERTION_SUCCESSFUL: 0,
+      ASSERTION_FAILED: 0,
+      ASSERTION_PERFORMANCE_PROBLEMS: 0,
+    }
+    for uuid, result in self.history.items():
+      data[result] += 1
+
+    # If no assertion raises exceptions, component is operational
+    if data[ASSERTION_FAILED] == 0 and data[ASSERTION_PERFORMANCE_PROBLEMS] == 0:
+      return COMPONENT_OPERATIONAL
+
+    # Only response problems have been raised
+    if data[ASSERTION_PERFORMANCE_PROBLEMS] > 0 and data[ASSERTION_FAILED] == 0:
+      return COMPONENT_RESPONSE_PROBLEMS
+
+    # There's an actual outage
+    # Complete outage
+    if data[ASSERTION_FAILED] > 0 and data[ASSERTION_SUCCESSFUL] == 0:
+      return COMPONENT_COMPLETE_OUTAGE
+
+    # Partial outage
+    if data[ASSERTION_FAILED] > 0 and data[ASSERTION_SUCCESSFUL] != 0:
+      return COMPONENT_PARTIAL_OUTAGE
--- monitor/worker.py
+++ monitor/worker.py
@@ -1,20 +1,24 @@
+from .exceptions import *
+from .status import *
 import threading
 import time
 import logging
 import shortuuid
 
 class WorkerThread(threading.Thread):
-  def __init__(self, assertion, interval=1*60):
+  def __init__(self, assertion, assertion_executed_event, lock, interval=1*60):
     super(WorkerThread, self).__init__()
     self.assertion = assertion
     self.interval = interval
+    self.assertion_executed_event = assertion_executed_event
+    self.lock = lock
     self.logger = logging.getLogger('monitor')
+    self.check_object()
 
   def run(self):
     """
     Called when thread is started.
     """
-    self.check_object()
     self.loop()
 
   def loop(self):
@@ -28,18 +32,40 @@ class WorkerThread(threading.Thread):
     i = 0
     while True:
       i += 1
-
       self.logger.info(
           "Running assertion {}/{}({}). Iteration: {}"
           .format(component_name, component_name, friendly_name, i)
       )
-      self.assertion.test()
-      self.logger.info(
-          "Assertion {}/{}({}) executed without errors. Iteration: {}"
-          .format(component_name, component_name, friendly_name, i)
-      )
+
+      result = self.run_assertion()
+
+      if result == ASSERTION_SUCCESSFUL:
+        self.logger.info(
+            "Assertion {}/{}({}) executed without errors. Iteration: {}"
+            .format(component_name, component_name, friendly_name, i)
+        )
+      if result == ASSERTION_FAILED:
+        self.logger.warning(
+            "Assertion {}/{}({}) failed. Iteration: {}"
+            .format(component_name, component_name, friendly_name, i)
+        )
+
+      self.lock.acquire()
+      self.assertion_executed_event(uuid=self.assertion.uuid, result=result)
+      self.lock.release()
 
       time.sleep(self.interval)
+
+  def run_assertion(self):
+    """
+    Run assertion test method and detect if it was or wasn't successful
+    """
+    try:
+      self.assertion.test()
+    except TestFailed:
+      return ASSERTION_FAILED
+
+    return ASSERTION_SUCCESSFUL
 
   def check_object(self):
     """
@@ -52,3 +78,7 @@ class WorkerThread(threading.Thread):
     friendly_name = getattr(self.assertion, "friendly_name", "")
     if not len(friendly_name):
       raise RuntimeError("Assertion object has no friendly_name.")
+
+    uuid = getattr(self.assertion, "uuid", "")
+    if not len(uuid):
+      raise RuntimeError("Assertion object has no uuid. You're probably missing the super call on your __init__ method.")
